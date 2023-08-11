db2ed47715258ee758e89ebdf88580437bb4f76a
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-08-11 02:15:25 +0200

    Update the constructor method for the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
 
+
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     """
-    Added a docstring for demo purposes
+    Base class for other classes, defines common attributes and methods.
     """
 
-    def __init__(self, id=None, created_at=None, updated_at=None):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the BaseModel instance.
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    settattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dict_instances = self.__dict__.copy()
@@ -26,3 +39,5 @@ class BaseModel:
         dict_instances['updated_at'] = self.updated_at.isoformat()
 
         return dict_instances
+
+
