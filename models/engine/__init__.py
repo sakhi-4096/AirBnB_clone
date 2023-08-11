53a0c323adf987911e8cc4b5154c6cd35686aa2f
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-08-11 12:37:11 +0200

    Mark directory as Python package directory

---
+++ models/engine/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Set in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serialize __objects to the JSON file (path: __file_path)."""
+        serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w") as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        """
+        Deserialize the JSON file to __objects.
+        Only if the JSON file (__file_path) exists; otherwise, do nothing.
+        """
+        try:
+            with open(self.__file_path, "r") as file:
+                serialized_objects = json.load(file)
+                for key, obj_dict in serialized_objects.items():
+                    class_name, obj_id = key.split('.')
+                    cls = globals()[class_name]
+                    self.__objects[key] = cls(**obj_dict)
+        except FileNotFoundError:
+            pass
