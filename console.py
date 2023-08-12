55f243b6473fd592a6ffd39aeeb17a5066ac146f
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-08-12 22:26:09 +0200

    Update the 'preloop' method in the 'HBNBCommand' class to include new classes

--- console.py
+++ console.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-
+"""Command interperter"""
 
 import cmd
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+import sys
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -33,6 +44,19 @@ class HBNBCommand(cmd.Cmd):
         Do nothing on an empty line.
         """
         pass
+
+    def preloop(self):
+        """Initialize the classes dictionary."""
+        self.classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+        }
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
